098f3633b9a08e2d0e45efaabc2445c133004e22
emailjohnthomascaballero <emailjohnthomascaballero@gmail.com>
2022-02-21 03:57:40 +0800

    add comments

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  //! SPLASH SCREEN
   FlutterNativeSplash.removeAfter(initialization);
+  //! SPLASH SCREEN </>
+
   runApp(const MyApp());
 }
 
+//! SPLASH SCREEN
 void initialization(BuildContext context) async {
   // This is where you can initialize the resources needed by your app while
   // the splash screen is displayed.  Remove the following example because
   // delaying the user experience is a bad design practice!
   // ignore_for_file: avoid_print
-  print('ready in 3...');
-  await Future.delayed(const Duration(seconds: 1));
-  print('ready in 2...');
-  await Future.delayed(const Duration(seconds: 1));
+  //print('ready in 3...');
+  //await Future.delayed(const Duration(seconds: 1));
+  //print('ready in 2...');
+  //await Future.delayed(const Duration(seconds: 1));
   print('ready in 1...');
   await Future.delayed(const Duration(seconds: 1));
   print('go!');
 }
+//! SPLASH SCREEN/ </>
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -40,7 +45,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Java Reviewer'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
