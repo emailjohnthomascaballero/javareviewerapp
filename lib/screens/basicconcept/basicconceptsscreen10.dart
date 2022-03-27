b6b9a5aa3a98cee2d7a5940e00ddaaf3967bb237
emailjohnthomascaballero <emailjohnthomascaballero@gmail.com>
2022-03-27 21:28:02 +0800

    screen 10

---
+++ lib/screens/basicconcept/basicconceptsscreen10.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import '/widgets/drawerwidget.dart';
+
+import 'basicconceptsscreen2.dart';
+
+class BasicConceptsScreen10 extends StatelessWidget {
+  const BasicConceptsScreen10({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: DrawerWidget(),
+      appBar: AppBar(
+        title: Text('BASIC CONCEPTS'),
+        backgroundColor: Color.fromARGB(255, 39, 39, 39),
+      ),
+      body: Container(
+        //! button
+        alignment: Alignment.bottomCenter,
+        padding: EdgeInsets.all(32),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            minimumSize: Size(300, 50),
+            primary: Color.fromARGB(255, 63, 63, 63),
+          ),
+          child: Text('NEXT'),
+          // proceed to next screen
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => BasicConceptsScreen2()));
+          },
+          // proceed to next screen </>
+        ),
+        //! button </>
+
+        //! background image
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/basicconcepts_screen_10.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        //! background image </>
+      ),
+    );
+  }
+}
